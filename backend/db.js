45e6c99a9387e17fd834b267ff2eb723743f570b
Muhammad Awais <awaisikram10@gmail.com>
2024-09-19 16:52:07 +0500

    backend seperated

--- backend/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const mongoURI ="mongodb://0.0.0.0/inotebook"
-
-const connectToMongo  = async()=>{
-    await mongoose.connect(mongoURI)
-    console.log('connected to mongo database')
-}
-module.exports = connectToMongo;
